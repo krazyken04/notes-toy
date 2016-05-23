676c07465241c61c16e2be76ff1540395afeb4b3
Ken Hanson <warpspiderx@gmail.com>
2016-05-23 13:43:46 -0500

    Prepping for 24 Hour sprint
    This is the showing off portion.

---
+++ 24hours/notes/scripts.js
@@ -0,0 +1,351 @@
+// Templates
+var noteTemplate,
+    listTemplate;
+var currentlyEditing = null;
+
+$(document).ready(function(){
+  // Item Template population
+
+  // Block Template Population
+  noteTemplate = $('#templateContainer .note').clone().wrap('<p>').parent().html();
+  listTemplate = $('#templateContainer .list').clone().wrap('<p>').parent().html();
+
+  // Autoresizing script grabbed from StackOverflow
+  // http://stackoverflow.com/questions/454202/creating-a-textarea-with-auto-resize
+  // Modified to take a param from jQuery so I can apply it to every textarea
+
+  var observe;
+  if (window.attachEvent) {
+      observe = function (element, event, handler) {
+          element.attachEvent('on'+event, handler);
+      };
+  }
+  else {
+      observe = function (element, event, handler) {
+          element.addEventListener(event, handler, false);
+      };
+  }
+  function initAutosize (text) {
+      function resize () {
+          text.style.height = 'auto';
+          text.style.height = text.scrollHeight+'px';
+      }
+      /* 0-timeout to get the already changed text */
+      function delayedResize () {
+          window.setTimeout(resize, 0);
+      }
+      observe(text, 'change',  resize);
+      observe(text, 'cut',     delayedResize);
+      observe(text, 'paste',   delayedResize);
+      observe(text, 'drop',    delayedResize);
+      observe(text, 'keydown', delayedResize);
+
+      text.focus();
+      text.select();
+      resize();
+  }
+
+  $('textarea').each(function(i, textarea){
+    initAutosize(textarea);
+  });
+
+  /* Interaction */
+
+  // Open Note
+  $('#closedNotePrompt, #noteButton').click(function(event){
+    $(this).parents().find('#notePromptWrapper').attr('class', 'open note');
+    $('#noteInput #noteTitle').trigger('focus');
+  });
+
+  // Open List
+  $('#listButton').click(function(){
+    event.stopPropagation();
+    $(this).parents().find('#notePromptWrapper').attr('class', 'open list');
+    $('#listInput #listTitle').trigger('focus');
+  });
+
+  // Bind up delete buttons
+  $(document).on('click', 'li i.fa-times', function(e){
+    e.stopPropagation();
+    if($(this).parents('ul').find('li').size() <= 1){
+      $(this).parents('ul').append(inputListItems);
+      $(this).parents('ul').find('li:last-child input').trigger('focus');
+    } else if($(this).parents('li').next().size() > 0) {
+      $(this).parents('li').next().find('input').trigger('focus');
+    } else if($(this).parents('li').prev().size() > 0){
+      $(this).parents('li').prev().find('input').trigger('focus');
+    }
+    $(this).parents('li').remove();
+  });
+
+  // Bind up Add Buttons
+  $(document).on('click', 'li i.fa-plus', function(e){
+    e.stopPropagation();
+    $(this).parents('ul').append(inputListItems);
+    $(this).parents('ul').find('li:last-child input').trigger('focus');
+  });
+
+  // Bind up future enter keys and handle list item duplication
+  $(document).on('keypress', 'input, textarea', function(e){
+    var key = e.which;
+    if(key == 13){
+      // Title Inputs
+      if($(this).parent().attr('id') === 'noteInput' || $(this).parent().attr('id') === 'listInput'){
+        $(this).parent().find('textarea').trigger('focus');
+        $(this).parent().find('li input').eq(0).trigger('focus');
+      } else if($(this).parents('li').size() > 0){
+        console.log('IN A LIST PRESSING ENTER!')
+        // List Item Inputs
+        $(this).parents('ul').append($('#inputListItems').clone().html());
+        $(this).parents('ul').find('li:last-child input').trigger('focus');
+      }
+    }
+  });
+
+  // Bind up focus
+  $(document).on('focus', 'li input', function(){
+    $(this).parents('ul').find('.focused').removeClass('focused');
+    $(this).parent().addClass('focused');
+  });
+
+  // Bind Click off
+  $(document).click(function(event){
+    // Clear Empty List Nodes
+    if($('#notePromptWrapper').hasClass('open') && $(event.target).parents('#notePromptWrapper').size() === 0){
+      // Lists
+      $('#listInput').find('li input').each(function(i, input){
+        if(!$(input).val() && i != 0){
+          $(input).parents('li').remove();
+        }
+      });
+
+      $('#notePromptWrapper').attr('class', 'closed');
+    }
+  });
+
+  $('#saveNote').click(function(){
+    save(false);
+  });
+
+  $('#saveList').click(function(){
+    save(true);
+  });
+
+  // Editing
+  $(document).on('click', '.entry', function(event){
+    event.stopPropagation();
+    var ID = $(this).attr('id');
+
+    var clickedData = _.find(Lockr.get('Zenenotes').notes, function(note){
+      if(note.id === ID){
+        console.log('DATA: ', note);
+        return true;
+      }
+    });
+
+    editNote(clickedData);
+  });
+
+  // initial render if data exists
+  var LockrData = Lockr.get('Zenenotes');
+
+  if(LockrData){
+    var sorted = _(LockrData.notes).sortBy(function(item){
+      return item.id;
+    });
+    $.each(sorted, function(i, note){
+      renderItems(note, false);
+    });
+  }
+});
+
+// Sample JSON Storage
+/*
+{
+  notes: [
+    {
+      id: 1234
+      type: note,
+      title: title,
+      content: paragraphs
+    },
+
+    {
+      id: 2345,
+      type: list,
+      title: title,
+      items: [string, string, string]
+    }
+  ]
+}
+*/
+
+function save(list){
+
+  // Can we even save?
+  var canSave;
+  if(list){
+    $('#listInput ul li').each(function(i, item){
+      if($(item).find('input').val().length > 0){
+        canSave = true;
+      }
+    });
+  } else {
+    if($('#noteBody').val().length > 0){
+      canSave = true;
+    }
+  }
+
+  if(!canSave){
+    return false;
+  }
+
+  // Build JSON
+  var entryItem = {};
+  if(!currentlyEditing){
+    entryItem.id = (new Date).getTime();
+  } else {
+    entryItem.id = currentlyEditing;
+    $('#'+currentlyEditing).remove();
+    $('body').trigger('click');
+  }
+
+  if(list){
+    var title = $('#listInput #listTitle').val();
+    var items = [];
+    $('#listInput li').each(function(i, item){
+      items.push($(item).find('input').val());
+    });
+
+    console.log('items found!', items);
+
+    if(title.length > 0){
+      entryItem.title = title;
+    }
+
+    entryItem.items = items;
+    entryItem.type = 'list';
+  } else {
+    var title = $('#noteInput #noteTitle').val();
+    var content = $('#noteBody').val();
+
+    if(title.length > 0){
+      entryItem.title = title;
+    }
+
+    entryItem.content = content;
+  }
+
+  renderItems(entryItem, true);
+}
+
+function renderItems(item, newItem){
+  if(item){
+    if(item.type === 'list'){
+      $('#notesWrapper').prepend(listTemplate);
+      var $newItem = $('#notesWrapper .entry:first-child');
+      $newItem.find('ul').empty();
+      console.log('RENDERING ITEMS: ', item.items);
+      $.each(item.items, function(i, itemText){
+        var clone = $('#templateContainer .listContainer li:first-child').clone().text(itemText);
+        $newItem.find('ul').append(clone);
+      });
+    } else {
+      $('#notesWrapper').prepend(noteTemplate);
+      var $newItem = $('#notesWrapper .note:first-child');
+      $newItem.find('.noteContents').text(item.content);
+    }
+
+    $newItem.attr('id', item.id);
+
+    if(item.title){
+      $newItem.find('h2').text(item.title);
+    } else {
+      $newItem.find('h2').text('');
+    }
+
+    // Update Storage only if new item and not on initial render
+    if(newItem){
+      createJSON();
+    }
+  }
+}
+
+function createJSON(){
+  var masterJSON = {
+    'notes' : []
+  }
+
+  $('#notesWrapper .entry').each(function(i, entry){
+    var entryObject = {};
+    var id,
+        type,
+        title;
+
+    var id = $(entry).attr('id');
+
+    if($(entry).find('ul').size() > 0){
+      type = 'list';
+    } else {
+      type = 'note'
+    }
+
+    title = $(entry).find('h2').text();
+
+    if(type === 'list'){
+      var items = [];
+      $(entry).find('li').each(function(i, item){
+        items.push($(item).text());
+      });
+      entryObject.items = items;
+    } else {
+      var content = $(entry).find('.noteContents').text();
+      entryObject.content = content;
+    }
+
+    entryObject.id = id;
+    entryObject.type = type;
+    if(title.length > 0) {
+      entryObject.title = title;
+    }
+
+    masterJSON.notes.push(entryObject);
+  });
+  console.log(masterJSON);
+  Lockr.set('Zenenotes', masterJSON);
+
+  if(currentlyEditing){
+    currentlyEditing = null;
+    $('.titleInput').val('');
+    $('#noteBody').val('');
+    $('#listInput ul').empty();
+    $('#listInput ul').append($('#inputListItems').clone().html());
+  }
+}
+
+function editNote(note){
+  currentlyEditing = note.id;
+
+  if(note.type === 'list'){
+    $('#notePromptWrapper').attr('class', 'open list');
+    $('#listTitle').trigger('focus');
+    $('#listTitle').val(note.title);
+    $.each(note.items, function(i, item){
+      console.log('ITEM: ', item);
+      if(i === 0){
+        $('#listInput ul li:first-child input').val(item);
+        $('#listInput ul').append($('#inputListItems').clone().html());
+        $('#listInput ul').find('li:last-child input').trigger('focus');
+      } else {
+        $('#listInput ul li:last-child input').val(item);
+        $('#listInput ul').append($('#inputListItems').clone().html());
+        $('#listInput ul').find('li:last-child input').trigger('focus');
+      }
+    });
+  } else if(note.type === 'note'){
+    $('#notePromptWrapper').attr('class', 'open note');
+    $('#noteInput #noteTitle').trigger('focus');
+    $('#noteTitle').val(note.title);
+    $('#noteBody').val(note.content);
+  }
+}
